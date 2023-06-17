f7d20bf31db7f6a023f60f41b8965b9f1844c9da
AhmedOredunni <oredunniahmed@gmail.com>
2023-06-17 02:29:35 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x, y;
+
+	for (x = 'a'; x <= 'z'; x++)
+	for (y = 'A'; y <= 'Z'; y++)
+	putchar(x);
+	putchar(y);
+	putchar('\n');
+	return (0);
+}
