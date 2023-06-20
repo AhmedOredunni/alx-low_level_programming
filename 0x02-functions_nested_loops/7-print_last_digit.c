0dad38e0346867cb3c1d4cb208e8c74f276f95d1
AhmedOredunni <oredunniahmed@gmail.com>
2023-06-20 19:56:10 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Output the last digit of a vlue
+ * @n: value to output the last digit
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	return ((n % 10));
+}
