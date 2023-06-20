ad2dcd9a70a4b8a4501f1dbc5539bfd494204925
AhmedOredunni <oredunniahmed@gmail.com>
2023-06-20 21:43:26 +0000

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-		        if (i != 98)
+			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
@@ -29,7 +29,7 @@ void print_to_98(int n)
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-			        printf("%d\n", j);
+				printf("%d\n", j);
 		}
 	}
 }
