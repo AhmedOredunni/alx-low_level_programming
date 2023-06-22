fc12b658e31db0186c8d884eaeae9a3436fba8bc
AhmedOredunni <oredunniahmed@gmail.com>
2023-06-22 11:04:50 +0000

    Multiply to numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two numbers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ * Return: return the product of the two number
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
