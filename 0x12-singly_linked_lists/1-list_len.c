fe76d43d4df1f74b734616b60bfb025a22192ff6
AhmedOredunni <oredunniahmed@gmail.com>
2023-07-27 11:02:56 +0000

    1-list_len.c created

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Find the length of the list
+ * @h: the pointer variable, pointing to the head
+ *
+ * Return: the length of the list
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
