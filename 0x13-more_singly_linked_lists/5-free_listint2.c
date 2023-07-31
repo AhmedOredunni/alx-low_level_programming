3f5ccca16ac82f40fe7287d2b6322d475a8e9548
AhmedOredunni <oredunniahmed@gmail.com>
2023-07-31 21:03:21 +0000

    error in task 5 fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,9 +8,9 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
-	while (temp != NULL)
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
